73b52467dcb0e1a4718e09ff4bf90ee6c3beb55c
ibrahim bkkari <ibrahimbkari9@gmail.com>
2026-01-03 23:32:13 +0000

    fix api

---
+++ api/contact.js
@@ -0,0 +1,7 @@
+export default function handler(req, res) {
+  res.status(200).json({
+    name: "Ibrahim Bkkari",
+    whatsapp: "0968201410",
+    github: "barhum915"
+  });
+}
---
+++ api/github.js
@@ -0,0 +1,10 @@
+export default async function handler(req, res) {
+  try {
+    const response = await fetch("https://api.github.com/users/barhum915");
+    const data = await response.json();
+
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
