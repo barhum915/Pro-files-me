b647069cf0a6558b07d5bfbe6600a814ad72366a
ibrahim bkkari <ibrahimbkari9@gmail.com>
2026-01-04 00:00:31 +0000

    fix api

--- api/github.js
+++ api/github.js
@@ -1,7 +1,11 @@
-module.exports = (req, res) => {
-  res.status(200).json({
-    name: "Ibrahim Bkkari",
-    whatsapp: "0968201410",
-    github: "barhum915",
-  });
+module.exports = async (req, res) => {
+  try {
+    const r = await fetch(
+      "https://api.github.com/users/barhum915/repos?sort=updated&per_page=6"
+    );
+    const data = await r.json();
+    res.status(200).json(data);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
 };
